a99110d8c3937d3d146baaab493bd6c2699d9a23
Meehanella <caloniaella@gmail.com>
2026-09-23 21:05:49 +0800

    Update SupabaseConnector.java

--- database/SupabaseConnector.java
+++ database/SupabaseConnector.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SupabaseConnector {
-    private static final String URL = "jdbc:postgresql://db.YOUR_PROJECT_REF.supabase.co:5432/postgres";
+    private static final String URL = "jdbc:postgresql://db.qihryxvenaozhaqswzxy.supabase.co:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "YOUR_SUPABASE_DB_PASSWORD";
+    private static final String PASSWORD = "UsTERbPBZj25EKr1";
 
     public static Connection connect() {
         try {
